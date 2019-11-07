6ac16c9bf3c09fbe64752416b1523c35c19cba60
Rory Murray <rory.christian.murray@gmail.com>
2019-11-06 19:21:11 -0500

    Setting up recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,37 @@
 
 import math
 
+r1 = {'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5}
+r2 = {'milk': 1288, 'flour': 9, 'sugar': 95}
+
+l1 = {'milk': 2, 'sugar': 40, 'butter': 20}
+l2 = dict([('milk', 5), ('sugar', 120), ('butter', 500), ])
+
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    print(f"recipe : {recipe}\n larder : {ingredients}\n")
+
+    # get k:v using items() method
+    for k, v in recipe.items():
+        print(f"k : {k}\nv : {v}\n")
+
+    # retrieve position index and corresponding value
+    # using the enumerate() function
+    for i, v in enumerate(recipe):
+        print(f"i : {i}\nv : {v}\n")
+
+    return 0
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+# if __name__ == '__main__':
+#     # Change the entries of these dictionaries to test
+#     # your implementation with different inputs
+#     recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+#     ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+#     print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+#         batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+
+
+test1 = recipe_batches(r1, l1)
+
+# test2 = recipe_batches(r2, l2)
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,6 +3,7 @@
 import argparse
 
 lister = [3, 9, 7, 1, 16, 12, 4]
+stonks = [100, 90, 80, 50, 20, 10]
 
 
 def find_max_profit(prices):
@@ -42,16 +43,18 @@ def find_max_profit(prices):
     return max_profit
 
 
-if __name__ == '__main__':
-    # This is just some code to accept inputs from the command line
-    parser = argparse.ArgumentParser(
-        description='Find max profit from prices.')
-    parser.add_argument('integers', metavar='N', type=int,
-                        nargs='+', help='an integer price')
-    args = parser.parse_args()
+# if __name__ == '__main__':
+#     # This is just some code to accept inputs from the command line
+#     parser = argparse.ArgumentParser(
+#         description='Find max profit from prices.')
+#     parser.add_argument('integers', metavar='N', type=int,
+#                         nargs='+', help='an integer price')
+#     args = parser.parse_args()
 
-    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
-        profit=find_max_profit(args.integers), prices=args.integers))
+#     print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+#         profit=find_max_profit(args.integers), prices=args.integers))
 
 
 test1 = find_max_profit(lister)
+
+test2 = find_max_profit(stonks)
